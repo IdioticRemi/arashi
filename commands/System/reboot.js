5eb7a159d0988a7e09835dc53889affda57b5814
BuildTools <unconfigured@null.spigotmc.org>
2018-12-01 22:50:45 +0100

    Updated random things

--- commands/System/reboot.js
+++ commands/System/reboot.js
@@ -4,6 +4,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
+			aliases: ['restart'],
 			permissionLevel: 10,
 			guarded: true,
 			description: language => language.get('COMMAND_REBOOT_DESCRIPTION')
